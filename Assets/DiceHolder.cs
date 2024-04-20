d3e46480a2f68cdf5960755b9ea32f6d0e8d1284
Bekir <bekirekiz@ogrenci.beykoz.edu.tr>
2024-04-20 17:41:24 +0300

    Somethiong

--- Assets/DiceHolder.cs
+++ Assets/DiceHolder.cs
@@ -4,17 +4,34 @@ using UnityEngine;
 
 public class DiceHolder : MonoBehaviour
 {
-    public CollectableDice CollectedDice;
+    public bool Fill;
     public int DiceHealth;
     [SerializeField] private SpriteRenderer _sp;
-
-
+    [SerializeField] private List<Dice> _dices = new List<Dice>(); // ilk dice Movement, ikinci dice Combnat, ucuncu dice Defence
+    [SerializeField] private Dice _myDice;
 
     public void ChangeDice(CollectableDice dice)
     {
         _sp.sprite = dice.DiceType.DiceSprite;
-        CollectedDice = dice;
+        Fill = true;
         DiceHealth = dice.DiceType.DiceHealth;
-        Destroy(dice.gameObject);
+        SetADice(dice.DiceType);
+    }
+    private void SetADice(DiceTypeSCB diceTypeSCB)
+    {
+        switch (diceTypeSCB.TypeOfDice)
+        {
+            case TypeOfDices.Movement:
+                _myDice = _dices[0];
+                break;
+            case TypeOfDices.Combnat:
+                _myDice = _dices[1];
+                break;
+            case TypeOfDices.Defence:
+                _myDice = _dices[2];
+                break;
+            default:
+                break;
+        }
     }
 }
